d00d8a11f525f18225689395c57b927b70dc3ae3
madhanmanik <madhanmanik@gmail.com>
2018-12-03 15:46:27 +0530

    s

--- src/main/java/com/uae/controller/WelcomeController.java
+++ src/main/java/com/uae/controller/WelcomeController.java
@@ -3,6 +3,9 @@ package com.uae.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -51,13 +54,10 @@ public class WelcomeController {
 	     return test;
 	    }
 	 
-	@ApiOperation(value = "View a list of itemlist products",response = List.class)
-	 @RequestMapping(value = "/itemlist", method= RequestMethod.GET)
-	    public List list(String name){
-		 List<String> test = new ArrayList<String>();
-		 test.add("idli");
-		 test.add("vada");
-	     return test;
+	@ApiOperation(value = "itemlist products",response = List.class)
+	 @RequestMapping(value = "/itemlist", method= RequestMethod.POST)
+	    public List < String > list(@RequestBody List<String>  name){
+	     return name;
 	    }
 	   
 
